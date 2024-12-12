5fa37063e02d232c18049704b60858ffab4a553e
Ashira Boonfongmanee <104916924+AsherS61@users.noreply.github.com>
2024-12-12 14:34:29 +0700

    Delete ChulaCPP directory

--- ChulaCPP/char/char.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    char c1 = 't', c2  ='h' , c3 = 'e';
-    cout << c1 << c2 << c3 << endl;
-}
--- ChulaCPP/hello_world/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World." << std::endl;
-  std::cout << "We're using C++." << std::endl;
-}
